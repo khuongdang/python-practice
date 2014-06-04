092ac68253cd4950ac6cb426ae0d2ec65c8a4827
unknown <khuongdang@HSSSC1PCL00488.HarveyNash.vn.local>
2014-06-04 11:05:50 +0700

    Update ex3 - Khuong

---
+++ ex3.py
@@ -0,0 +1,29 @@
+#Print the progam
+print "I will now count my chickens!"
+#Start print Hens number 
+print "Hens", 25 + 30 / 6
+#Start print Hens Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+print "I will count the eggs: "
+#Start print eggs number
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#Check the operator
+print "Is it true that 3 + 2 < 5 - 7?"
+#Print the result of operator
+print 3 + 2 < 5 - 7 
+#Print the result of operator to check why the result is false
+print "What is 3 + 2 ? ", 3 + 2
+print "What is 5 - 7 ? ", 5 - 7
+print "Oh, that's why it's false"
+#More print operators
+print "How about some more ?"
+print "Is it greater? ", 5 > -2
+print "Is it great or equal ? ", 5 >= -2
+print "Is it less or equal ? ", 5 <= -2
+# the % is get the surplus
+print "test %", 4 % 3 + 1
+
+
+
+
