9d6a1258d59e63f7b534ad5d29eb74a2a9bd8916
unknown <khuongdang@HSSSC1PCL00488.HarveyNash.vn.local>
2014-06-06 14:01:19 +0700

    Update new exercise - Khuong

---
+++ ex5.py
@@ -0,0 +1,19 @@
+name = 'Kent'
+age = 31 # not a lie
+height = 74 #inches
+weight = 180 #pounds
+eyes = 'blue'
+teeth = 'white'
+hair = 'black'
+test = 5/4
+print "let's talk about %s" %name
+print "He's %d inches tall" %height
+print "He's %d pounds weight" %weight
+print "He's got %s eyes, %s teeth and %s hair" %(eyes, teeth, hair)
+
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
+	
+print "test %r here" %test
+
+
