b140436eb2068a518f36ef2b565b860eb9740a75
unknown <khuongdang@HSSSC1PCL00488.HarveyNash.vn.local>
2014-06-06 14:46:16 +0700

    Update new exercise - Khuong Dang

---
+++ ex10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass \non kakaka
+"""
+
+print fat_cat
